f62d57f492511ce26dfc0d6105a4178164188554
WooodHead <jcwsw129@gmail.com>
2023-10-22 16:16:09 +0800

    chore: Update

--- src/pages/content/components/CodeBlockReact.tsx
+++ src/pages/content/components/CodeBlockReact.tsx
@@ -1,19 +1,39 @@
 
+import { useRef, useEffect, useState } from 'react';
 import { cn } from '../../../../utils/cn';
 import { REACT_EDIT_PATH, REACT_PREVIEW_PATH } from '../config';
+import LoadingBlock from '../../popup/popup/LoadingBlock';
 
 export default function CodeBlockReact({
   hash, className = '', children = null,
 }) {
+  const frameRef = useRef<any>();
+  const [loaded, setLoaded] = useState<any>(false);
+
   const previewPath = `${REACT_PREVIEW_PATH}#${hash}`
   const editPath = `${REACT_EDIT_PATH}#${hash}`
-
   const width = 600
   const height = 600
+
+  useEffect(() => {
+    if (frameRef.current) {
+      var iframe = frameRef.current;
+      iframe.onload = function () {
+        setLoaded(true)
+      };
+    }
+  }, [frameRef.current]);
+
   return <div className={cn('', className)}>
-    <iframe
-      style={{ width, height }}
-      // src={editPath}></iframe>
-      src={previewPath}></iframe>
-  </div>;
+    {!loaded && <div className='flex flex-col ac jc py-10 tc'>
+      <LoadingBlock height={'30px'}></LoadingBlock>
+      This may take several seconds...
+    </div>}
+    <div className={cn(loaded ? 'opacity-1' : 'opacity-0')}>
+      <iframe
+        ref={frameRef}
+        style={{ width, height }}
+        src={previewPath}></iframe>
+    </div>
+  </div >;
 }
--- src/pages/popup/popup/LoadingBlock.tsx
+++ src/pages/popup/popup/LoadingBlock.tsx
@@ -1,9 +1,37 @@
-import { Spin } from 'antd';
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { cn } from '../../../../utils/cn';
 
-export default function LoadingBlock() {
-  return <div className={`flex ac jc`}>
-    <Spin></Spin>
-  </div>;
+export function Spin({ className = '' }) {
+  return <svg className={cn(`animate-spin duration-700 h-[14px] w-[14px] text-indigo-600`, className)} width="48" height="48" viewBox="0 0 36 36" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <defs>
+      <linearGradient x1="0%" y1="100%" x2="100%" y2="100%" id="linearGradient-5">
+        <stop stopColor="currentColor" stopOpacity="0" offset="0%">
+        </stop>
+        <stop stopColor="currentColor" stopOpacity="0.50" offset="39.9430698%">
+        </stop>
+        <stop stopColor="currentColor" offset="100%">
+        </stop>
+      </linearGradient>
+    </defs>
+    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+      <rect fillOpacity="0.01" fill="none" x="0" y="0" width="36" height="36">
+      </rect>
+      <path d="M34,18 C34,9.163444 26.836556,2 18,2 C11.6597233,2 6.18078805,5.68784135 3.59122325,11.0354951" stroke="url(#linearGradient-5)" strokeWidth="4" strokeLinecap="round">
+      </path>
+    </g>
+  </svg>
 }
+
+
+const LoadingBlock = ({ height = '30vh' }) => {
+  return (
+    <div style={{
+      display: 'flex', alignItems: 'center', justifyContent: 'center',
+      width: '100%', height, overflow: 'hidden',
+    }}>
+      <Spin></Spin>
+    </div>
+  );
+};
+
+export default LoadingBlock;
